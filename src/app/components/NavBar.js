7ccf89e0019a66ed651e6312c68c732f0362ce32
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2026-01-17 23:02:09 +0000

    Add an initial NavBar component using a custom click outside toggle hook to close the nav when a link is clicked or outside the nav is clicked.

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,27 @@
+"use client"
+
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+
+const NavBar = () => {
+
+    const { expanded, setExpanded, ref} = useClickOutsideToggle();
+
+    return (
+        <Navbar expanded={expanded} ref={ref} expand="lg" className="bg-body-tertiary" fixed="top">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/" onClick={() => setExpanded(false)}>Home</Nav.Link>
+                        <Nav.Link href="#link" onClick={() => setExpanded(false)}>Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavBar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <NavBar />
         {children}
       </body>
     </html>
