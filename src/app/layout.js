3bc8e099851aa83d488c7d778deee51ce1a180d7
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2026-01-17 21:55:34 +0000

    Remove boostrap packages import, not needed.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
