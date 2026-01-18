971aaa4bcbe4f7b3d667f2473b8ccb18f33f72bc
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2026-01-18 23:20:38 +0000

    Change colours and styles on navbar and footer. Add an off white background to the body and main elements.

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -3,16 +3,17 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
 
     const { expanded, setExpanded, ref} = useClickOutsideToggle();
 
     return (
-        <Navbar expanded={expanded} ref={ref} expand="lg" className="bg-body-tertiary" fixed="top">
+        <Navbar expanded={expanded} ref={ref} className={styles.NavBar} expand="lg" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="#home">Kyle Mardell Web Studio</Navbar.Brand>
+                <Navbar.Toggle onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" className={`${styles.DropIcon}`} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="/" onClick={() => setExpanded(false)}>Home</Nav.Link>
--- src/app/components/PageFooter.js
+++ src/app/components/PageFooter.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./PageFooter.module.css";
 
 const PageFooter = () => {
     return (
-        <footer className="text-center my-4">
-            <p>Created and Maintained by Kyle Mardell</p>
+        <footer className={`text-center py-4 ${styles.PageFooter}`}>
+            <p className="m-0">Created and Maintained by Kyle Mardell</p>
         </footer>
     );
 };
